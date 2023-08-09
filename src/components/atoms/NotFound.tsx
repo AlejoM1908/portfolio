9de22462af1a1a14c88a8e4d4034f84ddeb8f29d
Daniel Alejandro Melo Nuvan <dmelo@unal.edu.co>
2023-08-08 23:56:19 -0500

    Adding the under construction page as main and customizing the 404
    Changes:
    - Adding the color accents and theme toogle button to 404
    - Deleting the pages and placing the under construction banner as main

--- src/components/atoms/NotFound.tsx
+++ src/components/atoms/NotFound.tsx
@@ -1,17 +1,39 @@
+import { ThemeToggle } from './ThemeToogle'
+
 interface NotFoundProps {
-  homePath: string;
+  homePath: string
+  className?: string
 }
 
-export function NotFound({ homePath }: NotFoundProps): JSX.Element {
-  return <div className="grid h-screen px-4 bg-white place-content-center">
-  <div className="text-center">
-    <h1 className="font-black text-gray-200 text-9xl">404</h1>
+export function NotFound({ homePath, className }: NotFoundProps): JSX.Element {
+  return (
+    <div
+      className={`grid h-screen px-4 bg-light dark:bg-dark items-center ${className}`}
+    >
+      <div className="text-center">
+        <div className="mb-14">
+          <ThemeToggle />
+        </div>
+        <h1 className="font-black text-tx-light dark:text-tx-dark text-9xl">
+          404
+        </h1>
 
-    <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Uh-oh!</p>
+        <p className="text-2xl font-bold tracking-tight text-pr-accent sm:text-4xl">
+          Uh-oh!
+        </p>
 
-    <p className="mt-4 text-gray-500">We can't find that page.</p>
+        <p className="mt-4 text-tx-light dark:text-tx-dark">
+          We can't find that page.
+        </p>
 
-    <a href={homePath} className="inline-block px-5 py-3 mt-6 text-sm font-medium text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:ring"> Go Back Home</a>
-  </div>
-</div>
+        <a
+          href={homePath}
+          className="inline-block px-5 py-3 mt-6 text-sm font-medium text-white bg-pr-accent rounded hover:bg-pr-muted focus:outline-none focus:ring"
+        >
+          {' '}
+          Go Back Home
+        </a>
+      </div>
+    </div>
+  )
 }
